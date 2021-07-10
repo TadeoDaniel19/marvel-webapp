a59b5cd6d3bfc2411edc3fab762a2a4cb8d5055c
TadeoDaniel19 <hector_lectric@hotmail.com>
2021-07-10 00:46:27 -0500

    cons: add constants for colors

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#E26376', // pink
+  secondary: '#6F6E6B', // gray
+  third: '#9A9895', // light gray
+  fourth: '#CBC9C6', // light light gray
+  fifth: '#FFFFFF', // white
+  sixth: '#6E6E6B', // gray for label
+};
+
+export default colors;
